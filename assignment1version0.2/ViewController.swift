cd6825472de2551047a91500b0aabdd10b3d1c3b
anfgy001 <anfgy001@mymail.unisa.edu.au>
2017-09-21 15:52:31 +0930

    commit of changes for user input conversion
    strings to doubles

--- assignment1version0.2/ViewController.swift
+++ assignment1version0.2/ViewController.swift
@@ -10,9 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wheelDiameterTB: UITextField!
+    
+    @IBOutlet weak var rpmTB: UITextField!
+
+    @IBOutlet weak var targetDistanceTB: UITextField!
+    
+    var diameterDouble:Double?;
+    
+    var rpmDouble:Double?;
+    
+    var targetDistanceDouble:Double?
+    
+    // need to convert text fields
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        displayAlert(userMessage: "test");
+    }
+    
+   /* func calculateTime(diameter: Double, rpm: Double, distance: Double) -> Double
+    {
+        
+        
+        return 0.0;
+    }   */
+    
+    func convertInputToDouble()
+    {
+        
+        //NOTE: Doesn't handle empty input!!!
+        let diameterText:String? = wheelDiameterTB.text;
+        diameterDouble = Double(diameterText!)!;
+        
+        let rpmText:String? = rpmTB.text;
+        rpmDouble = Double(rpmText!)!;
+        
+        let targetDistanceText:String? = targetDistanceTB.text;
+        targetDistanceDouble = Double(targetDistanceText!)!;
+    }
+    
+    func displayAlert(userMessage: String)
+    {
+        _ = UIAlertController(title: "Alert", message: userMessage, preferredStyle: .alert);
+    }
+    
+    
+    
+    @IBAction func calculateTime(_ sender: UIButton)
+    {
+    
+    
+        convertInputToDouble();
+        displayAlert(userMessage: "test");
+        print("clicked" + "\n");
+        
+    
+    }
+    
+    
+    func greet(person: String) {
+        print("Hello, \(person)!")
     }
 
     override func didReceiveMemoryWarning() {
