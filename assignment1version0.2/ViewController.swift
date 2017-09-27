8c560315211ce4c5a3750dad9e9dac796f4d8b4c
Gianni Anfiteatro <anfgy001@mymail.unisa.edu.au>
2017-09-27 12:14:40 +0930

    user validation 75% complete
    Checks correctly, just need to make sure it refreshes properly

--- assignment1version0.2/ViewController.swift
+++ assignment1version0.2/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var errorLabel: UILabel!
+    
     @IBOutlet weak var timeTB: UITextField!
     
     @IBOutlet weak var wheelDiameterTB: UITextField!
@@ -26,59 +28,139 @@ class ViewController: UIViewController {
     
     // NOTE: Conversions from .text to Double? WORK
     
+    /*
+        The main function of loading the view from the view controller
+        Sets the default values for the text fields (to 1)
+     
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        displayAlert(userMessage: "test");
+        //displayAlert(userMessage: "test");
         wheelDiameterTB.text = "1";
         rpmTB.text = "1";
         targetDistanceTB.text = "1";
+        errorLabel.isHidden = true;
     }
     
+    
+    /*
+    
+        Converts the text to optional string to deal with exceptions (no input) 
+        Changes to double to deal with values with decimals
+     */
     func convertInputToDouble()
     {
         
         //NOTE: Doesn't handle empty input!!!
-        let diameterText:String? = wheelDiameterTB.text;
-        diameterDouble = Double(diameterText!)!;
         
-        let rpmText:String? = rpmTB.text;
-        rpmDouble = Double(rpmText!)!;
+        if (wheelDiameterTB.text == "")
+        {
+            print("There is no wheel diameter!");
+            // break
+        }
+        else if (rpmTB.text == "")
+        {
+            print("There is no rpm!");
+            // break
+        }
+        else if (targetDistanceTB.text == "")
+        {
+            print("There is no target distance!");
+            // break
+        }
+        else
+        {
+            
+            let diameterText:String? = wheelDiameterTB.text;
+            let rpmText:String? = rpmTB.text;
+            let targetDistanceText:String? = targetDistanceTB.text;
+            
+            let diameterConversion = Double(diameterText!);
+            let rpmConversion = Double(rpmText!);
+            let targetDistanceConversion = Double(targetDistanceText!);
+            
+            
+            if (diameterConversion == nil || rpmConversion == nil || targetDistanceConversion == nil)
+            {
+                print("invalid number");
+                errorLabel.isHidden = false;
+                timeTB.backgroundColor = UIColor.red; //TODO: Test this
+                // break and redo
+            }
+            else
+            {
+                diameterDouble = Double(diameterText!)!;
+                rpmDouble = Double(rpmText!)!;
+                targetDistanceDouble = Double(targetDistanceText!)!;
+            }
+            
+            
+            
+            
+        }
         
-        let targetDistanceText:String? = targetDistanceTB.text;
-        targetDistanceDouble = Double(targetDistanceText!)!;
+        
+        
+        
+        
+        
+        
     }
+    
+    
+    /* NOT NEEDED
     
     func displayAlert(userMessage: String)
     {
         _ = UIAlertController(title: "Alert", message: userMessage, preferredStyle: .alert);
     }
+ 
+ */
     
     // button function
+    /*
+     
+     Action for button to calculate time given the double values of the inputs
+     
+     
+     */
     @IBAction func calculateTime(_ sender: UIButton)
     {
-    
+        
     
         convertInputToDouble();
-        //displayAlert(userMessage: "test");
         print("clicked" + "\n");
-        if (rpmDouble != nil)
+        
+        // doubles are nil when they don't work
+        // DO NOT CONVERT
+        
+        if (diameterDouble == nil || rpmDouble == nil || targetDistanceDouble == nil)
         {
+            timeTB.text = "";
+            // erroneous input
+        }
+        else
+        {
+            let theTime:Double? = timeCalculation(diameter: diameterDouble!, rpm: rpmDouble!, distance: targetDistanceDouble!);
             
+            print(theTime!);
+            
+            let timeString = String(describing: theTime);
+            
+            timeTB.text = timeString;
         }
-        else {
-            print("nil");
-        }
         
-        let theTime:Double? = timeCalculation(diameter: diameterDouble!, rpm: rpmDouble!, distance: targetDistanceDouble!);
         
-        print(theTime!);
-        
-        let timeString = String(describing: theTime);
-        
-        timeTB.text = timeString;
     }
     
+    /*
+     
+     Chose to have multiple variables instead of a sole variable with calculations 
+     to make sure that even if the language isn't strongly typed, coupled etc that 
+     the correct calculations will be shown also to deal with order of precedence
+     
+     */
     func timeCalculation(diameter: Double, rpm: Double, distance: Double) -> Double
     {
         // Time = Distance / ((pi * Diameter) / 60) * RPM)
@@ -92,6 +174,7 @@ class ViewController: UIViewController {
         return time;
     }
     
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
