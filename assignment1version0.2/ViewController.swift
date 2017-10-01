f940f3bbfb19226bc6cf0dd78b7bedfd1add7bb1
Gianni Anfiteatro <anfgy001@mymail.unisa.edu.au>
2017-10-01 16:44:33 +1030

    Imperial to Metric complete + complete commenting

--- assignment1version0.2/ViewController.swift
+++ assignment1version0.2/ViewController.swift
@@ -4,10 +4,25 @@
 //
 //  Created by Anfiteatro, Gianni - anfgy001 on 21/9/17.
 //  Copyright © 2017 Anfiteatro, Gianni - anfgy001. All rights reserved.
+// Uni email ID: anfgy001, Uni ID NO: 110169824
 //
 
 import UIKit
 
+/*
+ 
+ Many global variables are used, majority of them being UI elements on the application itself,
+ Other global variables are chosen to be made global as there are uses across the application for them and while getters and setters 
+ would be perhaps better coding practice, in the context of this application where some data is only used one time and is constantly being updated by the user,
+ getters and setters were not implemented. Example: timeString string is always being updated each time the calculate is being pressed and is only determined by user input.
+
+ ToString method was also not implemented as the data was not saved on any data store or data base, so a toString function need only store information already present on the screen anyway.
+ HashCode methods were not needed either.
+ 
+ Good programming practice was taken under consideration with this assignment, with carefully placed code blocks, detailed comments and also performance and reliability taken into consideration along the way.
+ 
+ 
+ */
 class ViewController: UIViewController {
     
     var diameterIsInMetres:Bool = true;
@@ -50,11 +65,7 @@ class ViewController: UIViewController {
     
     var timeString:String = "";
     
-    var wheelDiameterUnit:String = "";
     
-    var TargetDistanceUnit:String = "";
-    
-    // NOTE: Conversions from .text to Double? WORK
     
     /*
         The main function of loading the view from the view controller
@@ -64,17 +75,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //displayAlert(userMessage: "test");
-        
         wheelDiameterTB.text = "1";
-        
         rpmTB.text = "1";
-        
         targetDistanceTB.text = "1";
-        
         errorLabel.isHidden = true;
     }
-    
     
     /*
     
@@ -103,21 +108,29 @@ class ViewController: UIViewController {
         }
         else
         {
-            
             var validation = validateInput();
-            
         }
         
     }
     
-    //func co
     
+    /*
+     Complete user validation transferred to a single function
+     Variable definitions not type explicit in declaration so that printing values would be made easier
+     User validation is done by declaration of new variables based off of user input (text fields)
+     and from there more variables are created to deal with converting them from String to double
+     If the conversions amount to nil, indication that the variables entered were not valid and actions follow to make sure 
+     that user validation is maintained
+     Chose to return a boolean for conditionals as opposed to a void function as it makes it easier aswell for other functions to determine
+     if user input is completely validated
+    */
     func validateInput() -> Bool
     {
         let diameterText:String? = wheelDiameterTB.text;
         let rpmText:String? = rpmTB.text;
         let targetDistanceText:String? = targetDistanceTB.text;
         
+        // no type declaration means when they are printed out the "Optional()" tags do not appear, saving time and performance as opposed to trimming the strings
         let diameterConversion = Double(diameterText!);
         let rpmConversion = Double(rpmText!);
         let targetDistanceConversion = Double(targetDistanceText!);
@@ -153,12 +166,16 @@ class ViewController: UIViewController {
     
     
     
-    // button function
+    
     /*
      
      Action for button to calculate time given the double values of the inputs
      The type of units distance and diameter have are determined here after user input validation for performance purposes
-     
+     If the double values (diameterDouble etc) are nil equivalent of error in user validation, warning message outputted to console
+     If both the distance and diameter are of the same measurement unit (whether it would be metres, millimetres for metric or yards or feet for imperial)
+     then they do not need to be converted as the formula will always work so long as the distance and diameter are of the same measurement type
+     If they are not of the same measurement type, as a general rule the higher unit type gets converted to the lower unit type to ensure precise computations
+     At the end the global variables for diameter, distance and rpm are reset
      
      */
     @IBAction func calculateTime(_ sender: UIButton)
@@ -166,11 +183,9 @@ class ViewController: UIViewController {
         
     
         convertInputToDouble();
-        
-        print("DIAMETER DOUBLE IS \(diameterDouble)");
         if (diameterDouble == nil || rpmDouble == nil || targetDistanceDouble == nil)
         {
-            print("ERROR INPUT HERE");
+            print("ERROR: User Validation Error! Please choose real numbers!");
             // erroneous input
         }
         else // input is all good
@@ -187,8 +202,7 @@ class ViewController: UIViewController {
                 {
                     if(distanceIsInYards) // both in yards
                     {
-                        print("distance double = \(targetDistanceDouble!) + diameter double = \(diameterDouble!)");
-                        
+                            // Don't need to convert anything
                     }
                     else // diameter yards, distance feet
                     {
@@ -196,9 +210,7 @@ class ViewController: UIViewController {
                         
                         // convert diameter to feet
                         // 
-                        print("\n DIAMETER UNIT TEXT \(diameterUnit.text) \n DISTANCE UNIT TEXT \(distanceUnit.text) \n")
                         targetDistanceDouble = targetDistanceDouble! / 3;
-                        print("distance double = \(targetDistanceDouble!) + diameter double = \(diameterDouble!)");
                         bothDiameterAndDistanceSameUnit = true;
                     }
                 }
@@ -207,20 +219,17 @@ class ViewController: UIViewController {
                     if(distanceIsInYards) // diameter feet, distance yards
                     {
                         // 1 yard is 3 feet
-                        print("\n DIAMETER UNIT TEXT \(diameterUnit.text) \n DISTANCE UNIT TEXT \(distanceUnit.text) \n")
                         diameterDouble = diameterDouble! / 3;
-                        print("distance double = \(targetDistanceDouble!) + diameter double = \(diameterDouble!)");
                         bothDiameterAndDistanceSameUnit = true;
                     }
                     else // distance, diameter feet
                     {
-                        print("distance double = \(targetDistanceDouble!) + diameter double = \(diameterDouble!)");
+                        // No conversion needed
                     }
                 }
             }
             else // system is metric
             {
-                
                 
                 if (diameterIsInMetres)
                 {
@@ -237,7 +246,6 @@ class ViewController: UIViewController {
                         // distance = mm
                         // convert diameter to mm
                         diameterDouble = diameterDouble! * 1000;
-                        print("distance double = \(targetDistanceDouble!) + diameter double = \(diameterDouble!)");
                         bothDiameterAndDistanceSameUnit = true;
                         
                     }
@@ -275,17 +283,20 @@ class ViewController: UIViewController {
                 
                 totalRevolutionsTB.text = revsString;
             }
-            // IDEA: ROUND THE RESULT, IMPLEMENT LATER ON....
+            
         }
-        
-        
-        
         diameterDouble = nil;
         rpmDouble = nil;
         targetDistanceDouble = nil;
         
     }
     
+    /*
+    
+     This will convert the diameter from one measurement unit to another, for metric, it will convert metres to millimetres and vice versa
+     for imperial it will convert feet to yards and vice versa
+     
+    */
     @IBAction func diameterChange(_ sender: UIButton)
     {
         if (!systemIsMetric) // imperial - convert to foot or yard
@@ -325,7 +336,12 @@ class ViewController: UIViewController {
     }
     
     
-    
+    /*
+     
+     This will convert the distance from one measurement unit to another, for metric, it will convert metres to millimetres and vice versa
+     for imperial it will convert feet to yards and vice versa
+     
+     */
     @IBAction func distanceChange(_ sender: Any)
     {
         
@@ -362,11 +378,19 @@ class ViewController: UIViewController {
         
     }
     
+    /*
+     
+     User validation happens before the system is converted.
+     local variables are kept for abstraction purposes and to keep data consistency
+     once again, variables are kept abstract from type specific definitions to avoid printing inconsistencies to save time and improve performance
+     Conversions are done by multiplying the local variables and trasferring them
+     
     
+    */
     @IBAction func convertSystem(_ sender: Any)
     {
         // validate first
-        
+        print("Attempting to convert system...");
         
         if (validateInput())
         {
@@ -388,11 +412,10 @@ class ViewController: UIViewController {
         
         if (systemIsMetric) // convert to imperial
         {
-            
+            metricUpdate();
             // check the current unit and convert to yards
             if (distanceIsInMetres)
             {
-                
                 // convert distance to yards
                 // 1 yard = 0.9144 metres
                 // 1 yard = 914.4 millimetres
@@ -442,6 +465,9 @@ class ViewController: UIViewController {
         }
         else // convert back to metric
         {
+            
+            imperialUpdate();
+            
             if (distanceIsInYards)
             {
                 // convert distance to metres
@@ -515,7 +541,7 @@ class ViewController: UIViewController {
     /*
  
      Decided to scale up instead of down for performance purposes, 
-     using larger numbers would take more of a performance hit
+     using larger numbers would take more of a performance hit and also to ensure computations are correct
      
     */
     func totalRevolutionsCalculation(rpm: Double, timeInMs: Double) -> Double
@@ -527,6 +553,60 @@ class ViewController: UIViewController {
         let finalRevolutions = rpm * timeInMinutes;
         
         return finalRevolutions;
+    }
+    
+    /*
+ 
+     Checking the UILabels to determine what unit measurement the metric system is on and update changes within system
+     This is called specifically only when it is guaranteed that the system is already using the metric system
+ 
+    */
+    func metricUpdate()
+    {
+        if (diameterUnit.text == "M")
+        {
+            diameterIsInMetres = true;
+        }
+        else
+        {
+            diameterIsInMetres = false;
+        }
+        
+        if (distanceUnit.text == "M")
+        {
+            distanceIsInMetres = true;
+        }
+        else
+        {
+            distanceIsInMetres = false;
+        }
+    }
+    
+    /*
+     
+     Checking the UILabels to determine what unit measurement the imperial system is on and update changes within system
+     This is called specifically only when it is guaranteed that the system is already using the imperial system
+     
+     */
+    func imperialUpdate()
+    {
+        if (diameterUnit.text == "Y")
+        {
+            diameterIsInYards = true;
+        }
+        else
+        {
+            diameterIsInYards = false;
+        }
+        
+        if (distanceUnit.text == "Y")
+        {
+            distanceIsInYards = true;
+        }
+        else
+        {
+            distanceIsInYards = false;
+        }
     }
     
    
